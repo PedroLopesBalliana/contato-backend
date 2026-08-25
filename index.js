b8759462354b47cce3a470502adb33fe0df65899
PedroLopesBalliana <42103581+PedroLopesBalliana@users.noreply.github.com>
2026-08-25 00:50:03 -0300

    applisten2

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.post('/contato', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
